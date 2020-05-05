8d2e437e5cdc1be1bacedd3cb1b3489e5caa306f
Severin Mairinger <mai.sevi@hotmail.com>
2020-05-05 11:58:32 +0200

    altered typescript classes

--- node_server/Message.ts
+++ node_server/Message.ts
@@ -1,7 +1,32 @@
 ///<reference path="User.ts"/>
 class Message {
-    message_id: String;
-    sent_from: User;
+    private readonly _message_id: number;
+    private readonly _sent_from: User;
+    private readonly _content: String;
+    private readonly _timestamp: String;
+    private readonly _to: String;   //contains either the socked id or the specific room it was sent to
 
+    constructor(message_id: number, sent_from: User, content: String, timestamp: String, to: String) {
+        this._message_id = message_id;
+        this._sent_from = sent_from;
+        this._content = content;
+        this._timestamp = timestamp;
+        this._to = to;
+    }
 
+    get message_id(): number {
+        return this._message_id;
+    }
+
+    get sent_from(): User {
+        return this._sent_from;
+    }
+
+    get content(): String {
+        return this._content;
+    }
+
+    get timestamp(): String {
+        return this._timestamp;
+    }
 }
--- node_server/User.ts
+++ node_server/User.ts
@@ -1,21 +1,21 @@
 class User {
-    socket_id: String;
+    socket_id: String;  //empty if not connected
     user_id: String;
     username: String;
     private _messages: Array<Message>;
+    last_online: number; //contains the unix timestamp of which the user last logged out
 
     constructor(socket_id: String, user_id: String, username: String) {
         this.socket_id = socket_id;
         this.user_id = user_id;
         this.username = username;
+        this.last_online = 0;
         this._messages = new Array<Message>()
     }
-
 
     get messages(): Array<Message> {
         return this._messages;
     }
-
 
     set addMessage(value: Message) {
         this._messages.push(value);
