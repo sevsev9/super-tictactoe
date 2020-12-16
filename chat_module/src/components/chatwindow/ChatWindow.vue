087d77161c7e74ae65488ac418c4a7dc6a419dcc
Severin Mairinger <mai.sevi@hotmail.com>
2020-12-16 23:34:51 +0100

    added inputContainer component

--- chat_module/src/components/chatwindow/ChatWindow.vue
+++ chat_module/src/components/chatwindow/ChatWindow.vue
@@ -1,15 +1,36 @@
 <template>
-    <div>
-        
+    <div style="min-height: 800px; border: 2px solid green">
+        <div class="header"><h1>Header</h1></div>
+        <div class="channels"><h1>Channels</h1></div>
+        <div class="msg-container"><h1>Messages</h1></div>
+        <div class="footer"><input-container></input-container></div>
     </div>
 </template>
 
 <script>
+    import InputContainer from "@/components/chatwindow/subcomponents/InputContainer";
     export default {
-        name: "ChatWindow"
+        name: "ChatWindow",
+        components: {InputContainer}
     }
 </script>
 
 <style scoped>
+    .header {
+        border: 2px solid red;
+    }
 
+    .channels {
+        float: left;
+        display: inline-block;
+        border: 2px solid red;
+    }
+
+    .msg-container {
+        border: 2px solid red;
+    }
+
+    .footer {
+        border: 2px solid red;
+    }
 </style>
---
+++ chat_module/src/components/chatwindow/subcomponents/InputContainer.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <button class="emote-button">:)</button>
+        <b-input class="message-input" placeholder="Type a nice message :)"></b-input>
+        <b-button class="send-button">$3ND</b-button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "InputContainer"
+    }
+</script>
+
+<style scoped>
+    .emote-button {
+        border-radius: 10px;
+        display: inline-block;
+    }
+
+    .message-input {
+        display: inline-block;
+        width: 60%;
+        margin-left: 5%;
+    }
+
+    .send-button {
+        display: inline-block;
+        margin-left: 5%;
+    }
+</style>
